6729c46a2a00d93c2f7613ff2fd126e15a7ed115
HarshM0210 <harshvijayrbl@gmail.com>
2026-03-11 23:25:07 +0530

    Final Fix

--- Hypervolume/Quick Hypervolume With Projection.cpp
+++ Hypervolume/Quick Hypervolume With Projection.cpp
@@ -15,28 +15,27 @@ int m; //Dimensions
 //To Convert The Hyperocatant Number From Decimal To Binary
 vector<int> toBinary(int num)
 {
-    if (num < 0 || num >= (1 << m)) // Check Bounds
+    if (num < 0 || num >= (1 << m))
     {
         cout << "ERROR: INVALID HYPEROCTANT NUMBER " << num << endl;
         return vector<int>(m, 0);
     }
-    
-    vector<int>binary(m);
-    for (int i = m - 1; i >= 0; i--)
+    vector<int> binary(m);
+    for (int i = 0; i < m; i++)
     {
-        binary[i] = num % 2;
-        num = num / 2;
+        binary[m - 1 - i] = (num >> i) & 1;
     }
+
     return binary;
 }
 
 //To Convert The Hyperocatant Number From Binary To Decimal
-int toDecimal(vector<int>binary)
+int toDecimal(const vector<int>& binary)
 {
     int num = 0;
     for (int i = 0; i < m; i++)
     {
-        num = num * 2 + binary[i];
+        num = (num << 1) | binary[i];
     }
     return num;
 }
@@ -49,56 +48,57 @@ vector<int> obtainHyperoctant(const vector<int>& point,const vector<int>& pivot)
     //Determine The Corresponding Hyperoctant
     for (int i=0; i<m; i++)
     {
-        if (point[i] > pivot[i])
-            hyperoctant[i] = 1;
-        else
-            hyperoctant[i] = 0;
+        hyperoctant[i] = (point[i] > pivot[i]) ? 1 : 0;
     }
     
     return hyperoctant;
 }
 
+
 //To Obtain The Set Of Non-Dominated Points From The Population
 vector<vector<int>> nonDominatedPopulation(const vector<vector<int>>& population)
 {
-    int n=population.size();
-    vector<vector<int>>arr;
-    
+    int n = population.size();
+    vector<bool> dominated(n, false);
+
     for (int i = 0; i < n; i++)
     {
-        bool dominated = false;
+        if (dominated[i]) continue;
 
-        for (int j = 0; j < n; j++)
+        for (int j = i + 1; j < n; j++)
         {
-            if (j == i) continue;
+            if (dominated[j]) continue;
 
-            bool betterOrEqual = true;
-            bool strictlyBetter = false;
-            
-            //Assuming Maximization Problem
+            bool iBetter = false;
+            bool jBetter = false;
+
             for (int k = 0; k < m; k++)
             {
-                if (population[j][k] < population[i][k])
-                {
-                    betterOrEqual = false;
+                if (population[i][k] > population[j][k]) iBetter = true;
+                else if (population[i][k] < population[j][k]) jBetter = true;
+
+                if (iBetter && jBetter)
                     break;
-                }
-                if (population[j][k] > population[i][k])
-                        strictlyBetter = true;
             }
-            if (betterOrEqual && strictlyBetter)
+
+            if (!jBetter && iBetter)
+                dominated[j] = true;
+            else if (!iBetter && jBetter)
             {
-                dominated = true;
+                dominated[i] = true;
                 break;
             }
         }
-        if (!dominated)
-        {
-            arr.push_back(population[i]);
-        }
     }
-        
-    return arr;
+
+    vector<vector<int>> result;
+    result.reserve(n);
+
+    for (int i = 0; i < n; i++)
+        if (!dominated[i])
+            result.push_back(population[i]);
+
+    return result;
 }
 
 //To Calculate The Hypervolume Of A Point
@@ -107,7 +107,7 @@ int hypervolume(const vector<int>& reference,const vector<int>& point)
     int product=1;
     for(int i=0;i<m;i++)
     {
-        product=product*(point[i]-reference[i]); //Discard 0 To be Done, Discuss it in in the implementation section
+        product=product*(point[i]-reference[i]);
     }
     return product;
 }
@@ -129,10 +129,7 @@ vector<int> calculatePivot(const vector<vector<int>>& population, const vector<i
         }
     }
     
-    vector<int>pivot(m);
-    for (int i = 0; i < m; i++)
-        pivot[i] = population[index][i];
-    return pivot;
+    return population[index];
 }
 
 //To Add A Point To The List Of Points Present In The Particular Hyperoctant
@@ -147,12 +144,21 @@ vector<int> pointProjection(const vector<int>& current_hyperoctant,const vector<
     vector<int>projected_point(m);
     for (int i=0; i<m; i++)
     {
-        if(hypoct[i]==current_hyperoctant[i])
-            projected_point[i]=point[i];
-        else
-            projected_point[i]=pivot[i];
+        for (int i = 0; i < m; i++)
+            projected_point[i] = (hypoct[i] == current_hyperoctant[i]) ? point[i] : pivot[i];
     }
     return projected_point;
+}
+
+//To Calculate The Reference Of A Hyperoctant
+vector<int> calculateReference(const vector<int>& hyperoctant,const vector<int>& pivot,const vector<int>& reference)
+{
+    vector<int> temp_hyperoctant(m);
+    for (int j = 0; j < m; j++)
+    {
+        temp_hyperoctant[j] = (hyperoctant[j] == 0) ? reference[j] : pivot[j];
+    }
+    return temp_hyperoctant;
 }
 
 //To Calculate The Quick Hypervolume Of The Population
@@ -176,30 +182,13 @@ int quickHypervolume(const vector<vector<int>>& population,const vector<int>& re
         vector<int>pivot= calculatePivot(population,reference);
         
         //Total Number Of Hyperoctants Corresponding To A Pivot
-        int total_hyperoctants = (int)(pow(2, m));
+        int total_hyperoctants = 1 << m;
         
         //3-D Vector To Hold The List Of All The Points Belonging To A Particular Hyperoctant
         vector<vector<vector<int>>> population_hyperoctant(total_hyperoctants);
         
         //2-D Vector To Hold The Reference Points For Each Of The Hypeoctants
         vector<vector<int>>reference_hyperoctant(total_hyperoctants);
-        
-        //Calculate The Reference Point For Each Of The Hyperoctants
-        for (int i = 0; i < total_hyperoctants; i++)
-        {
-            vector<int>hyperoctant=toBinary(i);
-            vector<int> temp_hyperoctant(m);
-
-            for (int j = 0; j < m; j++)
-            {
-                if (hyperoctant[j] == 0)
-                    temp_hyperoctant[j] = reference[j];
-                else
-                    temp_hyperoctant[j] = pivot[j];
-            }
-                
-            reference_hyperoctant[i] = temp_hyperoctant;
-        }
         
         //Iterate Through All The Points Of The Given Population
         for (int i=0; i<n; i++)
@@ -211,6 +200,12 @@ int quickHypervolume(const vector<vector<int>>& population,const vector<int>& re
                 
                 //Determine The Decimal Representation Of The Hyperoctant Found In The Previous Step
                 int hyperoctant_number=toDecimal(current_hyperoctant);
+                
+                //Calculate The Reference Point For The Hyperoctant To Which The Point Belongs
+                if (reference_hyperoctant[hyperoctant_number].empty())
+                {
+                    reference_hyperoctant[hyperoctant_number] = calculateReference(current_hyperoctant,pivot,reference);
+                }
                 
                 //Add The Point To The List Of Points Present In That Hyperoctant
                 addPoint(population_hyperoctant,hyperoctant_number,population[i]);
@@ -224,7 +219,7 @@ int quickHypervolume(const vector<vector<int>>& population,const vector<int>& re
                 }
                 
                 //Meaningfully Project The Point To All Other Required Hyperoctants
-                if (count > 1) //Refrain from projecting points of adjacent hyperoctants
+                if (count > 1) //Refrain From Projecting Points Of Adjacent Hyperoctants
                 {
                     vector<int>I(count);
                     int index_I=0;
@@ -233,7 +228,7 @@ int quickHypervolume(const vector<vector<int>>& population,const vector<int>& re
                         if(current_hyperoctant[j]==1)
                             I[index_I++] = j;
                     }
-                    int total=pow(2,count); // If There Are y 1s In The Binary Representation Of The Hyperoctant, Calculate: 2^y.
+                    int total = 1 << count; // If There Are y 1s In The Binary Representation Of The Hyperoctant, Calculate: 2^y.
                     
                     for (int mask=1; mask<total; mask++)
                     {
@@ -249,6 +244,12 @@ int quickHypervolume(const vector<vector<int>>& population,const vector<int>& re
                         
                         //Determine The Decimal Representation Of The Hyperoctant
                         hyperoctant_number=toDecimal(hypoct);
+                        
+                        //Calculate The Reference Point For The Hyperoctant To Which The Point Is Projected
+                        if (reference_hyperoctant[hyperoctant_number].empty())
+                        {
+                            reference_hyperoctant[hyperoctant_number] = calculateReference(hypoct,pivot,reference);
+                        }
                         
                         //Add The Point To The List Of Points Present In The Hyperoctant
                         addPoint(population_hyperoctant,hyperoctant_number,projected_point);
@@ -293,12 +294,9 @@ int main()
         
         //Non-Dominated Population
         vector<vector<int>>population= nonDominatedPopulation(population_initial);
-        int n_nonDominated=population.size();
         
         //Initial Reference
-        vector<int>reference(m);
-        for(int j=0;j<m;j++)
-            reference[j]=0;
+        vector<int>reference(m,0);
         
         //Calculating Hypervolume For The Non Dominated Popultaion
         int Hypervolume=quickHypervolume(population, reference);
